213ad7af33fb321d0df29449369e81e3c63c3b7c
rudra <rudra8997@gmail.com>
2022-12-20 06:53:54 +0000

    export-multipleValues-named

---
+++ exportMultipleValues.mjs
@@ -0,0 +1,12 @@
+const myArray = ["camel", 265, true, "5.6"];
+
+const bulb = { watts: 10, type: "LED" };
+
+const multiplyByFour = function (number) {
+  //let multiply;
+  //if (number.isDigit) {
+  let multiply = number * 4;
+  //}
+  return multiply;
+};
+export { myArray, bulb, multiplyByFour };
